b79bb7fde9e0d17ace3f953ca76d9ca66efcccf3
shubh0906 <shubham_0906@yahoo.com>
2018-12-10 20:56:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import TextArea from "./TextArea";
+import socketIOClient from "socket.io-client";
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: [],
+      endpoint: "http://127.0.0.1:8000",
+      num:0
+    };
+  }
+  componentDidMount() {
+    const { endpoint } = this.state;
+    console.log("cdm")
+    const socket = socketIOClient(endpoint);
+    socket.on('num',data => this.setState({num:data}))
+    socket.on("broadcast", data => {
+      console.log("response "+JSON.stringify(data)+" "+data.content);
+      let arr = [...this.state.response];
+      arr[data.file] = data.content;
+      this.setState({ response: arr });
+    });
+    
+  }
   render() {
+    const { response } = this.state;
+    console.log(this.state);
+    const display = this.state.response.map(elem => <textarea>{elem}</textarea>);
+    console.log(display);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ textAlign: "center" }}>
+        {
+          this.state.response.map(elem => {
+            return <textarea style={{width:'300px',height:'500px'}}value={elem}></textarea>
+          })
+        }
       </div>
     );
   }
 }
-
 export default App;
